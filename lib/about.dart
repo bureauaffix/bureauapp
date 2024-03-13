524babe6a1c58744c811421d93859d5dbd429363
developerujjwal <developer.ujjwalsharma@gmail.com>
2024-03-13 17:07:57 +0530

    app

--- lib/about.dart
+++ lib/about.dart
@@ -11,10 +11,103 @@ class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
         title: Text(
           'About',
           style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'About Us: Fostering Collaboration Through Technology',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Bureau Affix empowers your organization to thrive in a collaborative work environment. We offer a comprehensive suite of features designed to streamline communication, teamwork, and project management.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Here\'s how Bureau Affix breaks down the barriers to effective collaboration:',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Centralized Communication Platform: Unify your team\'s communication with features like instant messaging, group chats, and threaded discussions. Bureau Affix fosters a space for real-time exchange of ideas, information, and updates, keeping everyone on the same page.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Effortless Video Conferencing: Facilitate seamless virtual meetings regardless of location. Bureau Affix\'s video conferencing capabilities enable productive face-to-face interaction, fostering stronger connections and team spirit.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Collaborative Workspaces: Promote focused teamwork through the creation of project-specific groups. Bureau Affix allows you to organize teams around specific initiatives, ensuring everyone has the resources and information they need to succeed.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Streamlined Meeting Management: Simplify scheduling and organization with integrated calendar tools and availability checks. Bureau Affix eliminates the hassle of coordinating schedules, allowing teams to focus on what matters most – getting things done.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Secure File Sharing: Ensure everyone has access to the latest information. Bureau Affix provides a secure platform for sharing documents, presentations, and other files across departments, keeping your team informed and aligned.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Integrated Task Management: Assign tasks, track progress, and collaborate effectively on projects. Bureau Affix\'s built-in task management features offer a transparent view of individual and team workloads, promoting accountability and driving project completion.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Bureau Affix is more than just a collection of tools – it\'s a transformative solution designed to empower your teams and unlock their full potential.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'By incorporating Bureau Affix into your workflow, you can:',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Boost team productivity and efficiency',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Enhance communication and collaboration',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Improve project management and visibility',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Foster a more engaged and connected workforce',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Download Bureau Affix today and experience the future of collaborative work!',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/contact.dart
+++ lib/contact.dart
@@ -1,14 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 class Contact extends StatefulWidget {
-  static String p='contact';
+  static String p = 'contact';
 
   @override
   State<Contact> createState() => _ContactState();
 }
 
 class _ContactState extends State<Contact> {
+  // Method to launch URLs
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  // Custom button style
+  final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+    onPrimary: Colors.white,
+    primary: Colors.black,
+    minimumSize: Size(88, 36),
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(2)),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(backgroundColor: Colors.black,title: Text('Contact',style: TextStyle(color: Colors.white),),),);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Contact Us',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            // Social Media Buttons
+            ElevatedButton.icon(
+              style: buttonStyle,
+              icon: Icon(Icons.camera_alt_outlined),
+              label: Text('Instagram'),
+              onPressed: () => _launchURL('https://www.instagram.com/bureauaffix/'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton.icon(
+              style: buttonStyle,
+              icon: Icon(Icons.chat_bubble_outline),
+              label: Text('Twitter'),
+              onPressed: () => _launchURL('https://twitter.com/BureauAffix'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton.icon(
+              style: buttonStyle,
+              icon: Icon(Icons.facebook),
+              label: Text('Facebook'),
+              onPressed: () => _launchURL('https://www.facebook.com/yourusername'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton.icon(
+              style: buttonStyle,
+              icon: Icon(Icons.video_library_outlined),
+              label: Text('YouTube'),
+              onPressed: () => _launchURL('https://www.youtube.com/@bureauaffix/featured'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton.icon(
+              style: buttonStyle,
+              icon: Icon(Icons.email_outlined),
+              label: Text('Email'),
+              onPressed: () => _launchURL('mailto:your.bureauaffix22@gmail.com'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
